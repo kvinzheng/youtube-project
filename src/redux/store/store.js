1097437a272406a6231e9d3dc27535599bdff675
Kevin(Jiahong) Zheng <kvinzheng@gmail.com>
2017-10-08 14:50:05 -0700

    finish Api tests

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,8 +1,9 @@
 import thunkMiddleware from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
-import * as Api from '../utils/api';
+import * as Api from '../utils/Api';
 import rootReducer from '../reducers';
 
+// store creation
 const store = createStore(
   rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
